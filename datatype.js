4adb37a2ca0a778771610e890e47fcdab4c4b3f0
dhanuganjave <dhananjayganjave96@gmail.com>
2023-08-08 09:00:21 +0530

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,23 @@
+let score="true"
+//console.log(typeof(score))
+//console.log(typeof(score))
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+let isLoggedIn="dhanu"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log (booleanIsLoggedIn)
+//1=>true.0=>false;
+//""=>false
+//"dhanu"==>true
+ 
+
+
+let someNumber=44
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
---
+++ first.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log(3+3)  //code redability should be high
+console.log("dhananajy")
+let name="dhananjay"
+let age=23
+let isLoggedIn=false
+
+//data types
+/* number=2to power 53
+big int
+string
+boolean =>true/false
+null=>standalone value is a object
+undefined
+symbol=>unique
+*/
+
+//object
+
+console.log(typeof null);
