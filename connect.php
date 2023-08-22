44393c311d25dd2691f492697e7f09ad8713cb84
dhanuganjave <dhananjayganjave96@gmail.com>
2023-08-22 15:32:44 +0530

    add

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$firstname= $_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email= $_POST['email'];
+$gender=$_POST['gender'];
+$password=$_POST['password'];
+$city=$_POST['city'];
+echo $firstname;
+echo $lastname;
+echo $email;
+echo $gender;
+echo $city;
+
+
+//database connnection
+
+?>
